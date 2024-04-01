28e7209f6e19abf38a2f41591ed895446197b2a3
Alex <88356153+Alexxmfs@users.noreply.github.com>
2024-04-01 10:29:42 -0300

    lista dupla

---
+++ 3° Semestre/Estruta de dados/lista_dupla/src/App.java
@@ -0,0 +1,14 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        
+        Lista lista = new Lista();
+
+        lista.inserirFinal("Selmini", 1);
+        lista.inserirFinal("Sandra", 2);
+        lista.inserirInicio("Surian", 3);
+        lista.inserirInicio("Flávio", 4);
+
+        // System.out.println(lista.pesquisar(3));
+        System.out.println(lista.remover(1));
+    }
+}
--- 3° Semestre/Estruta de dados/lista_dupla/src/Lista.java
+++ 3° Semestre/Estruta de dados/lista_dupla/src/Lista.java
@@ -3,8 +3,79 @@ public class Lista {
     int tamanho;
 
     // método para inserir um elemento no final da lista
+    public void inserirFinal(String nome, int ra){
+        No aux = new No(nome, ra);
+
+        if(tamanho == 0){
+            inicio = aux;
+            fim = aux;
+        } else {
+            fim.dir = aux;
+            aux.esq = fim;
+        }
+        fim = aux;
+        tamanho++;
+    }
+
 
     // método para inserir um elemento no inicio da lista
+    public void inserirInicio(String nome, int ra){
+        No aux = new No(nome, ra);
+        
+        if(tamanho == 0){
+            inicio = aux;
+            fim = aux;
+        } else {
+            aux.dir = inicio;
+            inicio.esq = aux;
+            inicio = aux;
+        }
+        tamanho++;
+    }
 
     // método para pesquisar um elemento (deverá ser retornado o endereço do elemento)
+    public No pesquisar(int ra){
+        No aux = inicio;
+        while(aux != null){
+            if(aux.ra == ra){
+                return aux;
+            }
+            aux = aux.dir;
+        }
+        return null;         
+    } 
+
+    // método para remover um elemento da lista
+    public boolean remover(int ra){
+        No aux = pesquisar(ra);
+        boolean status = false;
+
+        if(aux != null){
+            // caso 1 --> lista tem apenas um elemento
+            if(tamanho == 1){
+                inicio = null;
+                fim = null;
+            } 
+            else if(aux == inicio){ // caso 2 --> remoção no inicio
+                aux.dir = null;
+                aux.dir.esq = null;
+                inicio = aux.dir;
+            }
+            else if(aux == fim){ // caso 3 --> remoção  no final da lista
+                aux.esq.dir = null;
+                fim = aux.esq;
+                aux.esq = null;
+            } 
+            else { // caso 4 --> remoção de um elemento no meio da lista
+                aux.esq.dir = aux.dir;
+                aux.dir.esq = aux.esq;
+                aux.esq = null;
+                aux.dir = null;
+            }
+            aux = null;
+            status = true;
+            tamanho--;
+        }
+        return status;
+    }
 }
