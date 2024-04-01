bd702489902a13ee4783c6d922ca4d3862f213f7
Alex <88356153+Alexxmfs@users.noreply.github.com>
2024-04-01 11:02:53 -0300

    teste generico

---
+++ 3° Semestre/Estruta de dados/TesteGenerico/src/App.java
@@ -0,0 +1,8 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        
+        Numero<Double> n = new Numero<Double>();
+
+        n.imprimir();
+    }
+}
---
+++ 3° Semestre/Estruta de dados/TesteGenerico/src/Numero.java
@@ -0,0 +1,8 @@
+public class Numero<T> {
+    T x;
+    T y;
+
+    public void imprimir(){
+        System.out.println(x + "\n" + y);
+    }
+}
