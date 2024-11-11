152ebf06355ff0b2b70d439c7f0a86ef948492d0
Alexxmfs <alexxmfs@hotmail.com>
2024-11-11 11:47:05 -0300

    subindo api fast

---
+++ 4° Semestre/Analise-Exploratoria/fastAPI/main.py
@@ -0,0 +1,52 @@
+from fastapi import FastAPI
+from fastapi.responses import JSONResponse
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker, relationship
+from sqlalchemy import Column, Integer, String, ForeignKey, DateTime
+
+app = FastAPI()
+
+engine = create_engine('mysql://root:root@localhost:3307/aulaDB')
+
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+session = SessionLocal()
+
+Base = declarative_base()
+
+
+class Author(Base):
+    __tablename__ = 'authors'
+    
+    id = Column(Integer, primary_key=True)
+    name = Column(String)
+    age = Column(Integer)
+    
+class Post(Base):
+    __tablename__ = 'posts'
+    
+    id = Column(Integer, primary_key=True)
+    text = Column(String)
+    created = Column(DateTime)
+    authorid = Column(Integer, ForeignKey('authors.id', ondelete='CASCADE'))
+    
+Base.metadata.create_all(bind=engine)
+
+@app.post('/authors')
+def create_author(name: str, age: int):
+    
+    if name is None:
+        response = {'error': 'name is required'}
+        status_code = 400
+
+    if age is None:
+        response = {'error': 'age is required'}
+        status_code = 400
+        
+    author = Author(name=name, age=age)
+    session.add(author)
+    session.commit()
+    response = {'id': author.id, 'name': author.name, 'age': author.age}
+    status_code = 200
+    
+    return JSONResponse(content=response, status_code=status_code)
